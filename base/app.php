24618dc98422c3c37e8cfe2d8f1dff66f0613f01
Cees-Jan Kiewiet <ceesjank@gmail.com>
2021-07-11 15:34:51 +0200

    Use global loop accessor

--- base/app.php
+++ base/app.php
@@ -3,6 +3,7 @@
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
+use React\EventLoop\Loop;
 use React\Http\Message\Response;
 use React\Http\Server as HttpServer;
 use ReactInspector\Collector\Merger\CollectorMergerCollector;
@@ -22,7 +23,7 @@ use const WyriHaximus\FakePHPVersion\CURRENT;
 require 'vendor/autoload.php';
 
 $indexHtml = \Safe\file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'index.html');
-$loop = new LoopDecorator(Factory::create());
+Loop::set(new LoopDecorator(Loop::get()));
 
 $metrics = [];
 $middleware = [];
@@ -41,9 +42,9 @@ $middleware[] = $middlewareCollectornotFound;
 $metricsMiddleware[] = $middlewareCollectorMetrics;
 
 $metricsMiddleware[] = new PrinterMiddleware(new PrometheusPrinter(), new Metrics(
-    $loop,
+    Loop::get(),
     3,
-    new LoopCollector($loop),
+    new LoopCollector(Loop::get()),
     new MemoryUsageCollector(),
     new IOCollector(),
     new CollectorMergerCollector(
@@ -62,41 +63,37 @@ $middleware[] = function (ServerRequestInterface $request) use ($indexHtml): Res
     );
 };
 
-$server = new HttpServer($loop, ...$middleware);
+$server = new HttpServer(...$middleware);
 $server->on('error', static function (Throwable $throwable): void {
     echo $throwable, PHP_EOL;
 });
 
-$socket = new React\Socket\Server('0.0.0.0:6969', $loop, ['backlog' => 511]);
+$socket = new React\Socket\Server('0.0.0.0:6969', null, ['backlog' => 511]);
 $socket->on('error', static function (Throwable $throwable): void {
     echo $throwable, PHP_EOL;
 });
 $server->listen($socket);
 
-$metricsServer = new HttpServer($loop, ...$metricsMiddleware);
+$metricsServer = new HttpServer(...$metricsMiddleware);
 $metricsServer->on('error', static function (Throwable $throwable): void {
     echo $throwable, PHP_EOL;
 });
 
-$metricsSocket = new React\Socket\Server('0.0.0.0:9696', $loop, ['backlog' => 511]);
+$metricsSocket = new React\Socket\Server('0.0.0.0:9696', null, ['backlog' => 511]);
 $metricsSocket->on('error', static function (Throwable $throwable): void {
     echo $throwable, PHP_EOL;
 });
 $metricsServer->listen($metricsSocket);
 
-$signalHandler = function () use (&$signalHandler, $socket, $metricsSocket, $loop) {
+$signalHandler = function () use (&$signalHandler, $socket, $metricsSocket) {
     echo 'Caught signal', PHP_EOL;
-    $loop->removeSignal(SIGINT, $signalHandler);
-    $loop->removeSignal(SIGTERM, $signalHandler);
+    Loop::removeSignal(SIGINT, $signalHandler);
+    Loop::removeSignal(SIGTERM, $signalHandler);
     $socket->close();
     $metricsSocket->close();
     echo 'Closed and stopped everything', PHP_EOL;
-    $loop->stop();
+    Loop::stop();
 };
 
-$loop->addSignal(SIGINT, $signalHandler);
-$loop->addSignal(SIGTERM, $signalHandler);
-
-echo 'Loop::run()', PHP_EOL;
-$loop->run();
-echo 'Loop::stop()', PHP_EOL;
+Loop::addSignal(SIGINT, $signalHandler);
+Loop::addSignal(SIGTERM, $signalHandler);
